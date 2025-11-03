9b188f870776ac19551dc5947865b5c0b7ea561c
JK-22025 <kazanj7@gmail.com>
2025-11-02 19:44:30 -0500

    Initial Commit

---
+++ Fitness app/Fitness_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_appApp.swift
+//  Fitness app
+//
+//  Created by Jawad Kazan on 2025-11-02.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
